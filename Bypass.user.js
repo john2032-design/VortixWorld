dbad59b513a679cf2a3de8a952679a46cfb114fb
John <faysal.heshmatullah@gmail.com>
2025-12-21 10:39:16 -0400

    Increase interval duration from 1000ms to 1200ms

--- Bypass.user.js
+++ Bypass.user.js
@@ -562,7 +562,7 @@
                                     clearInterval(intervalId);
                                     try { if (onFinish) onFinish(); } catch (e) {}
                                 }
-                            }, 1000);
+                            }, 1200);
                             return { stop: () => clearInterval(intervalId) };
                         };
                         const spinnerCSS = `
